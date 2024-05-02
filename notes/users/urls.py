cf8c341cb36d5a0e37231fec5bba7bbdcff15931
Roman_M <prominb@gmail.com>
2024-05-02 18:18:41 +0300

    edit urls e015-1

--- notes/users/urls.py
+++ notes/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', views.signupuser, name='signup'),
     path('login/', views.loginuser, name='login'),
+    path('logout/', views.logoutuser, name='logout'),
 ]
