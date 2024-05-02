f47e6170aaf808ec88d56d7cc90973ce2b2cc338
Roman_M <prominb@gmail.com>
2024-05-02 18:17:46 +0300

    edit to ADD login loguot e015

--- notes/users/forms.py
+++ notes/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
@@ -18,3 +18,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+
+
+class LoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- notes/users/views.py
+++ notes/users/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 
 # Create your views here.
@@ -17,3 +20,23 @@ def signupuser(request):
             return render(request, 'users/signup.html', context={"form": form})
 
     return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+def loginuser(request):
+    if request.user.is_authenticated:
+       return redirect(to='noteapp:main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='noteapp:main')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='noteapp:main')
